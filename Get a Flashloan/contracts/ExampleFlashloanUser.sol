384096ac0d266f8517462f0dbd6314fa0901bfe8
unknown <>
2021-07-26 14:07:59 +0530

    flashloans

---
+++ Get a Flashloan/contracts/ExampleFlashloanUser.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.7.3;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import './FlashloanProvider.sol';
+import './IFlashloanUser.sol';
+
+contract ExampleFlashloanUser is IFlashloanUser {
+  function startFlashloan(
+    address flashloan,
+    uint amount,
+    address token
+  )
+    external
+  {
+    FlashloanProvider(flashloan).executeFlashloan(
+      address(this),
+      amount,
+      token,
+      bytes('')
+    );
+  }
+
+  function flashloanCallback(
+   uint amount,
+   address token,
+   bytes memory data
+ )
+   override
+   external
+ {
+   //do some arbitrage, liquidation, etc..
+
+   //Reimburse borrowed tokens
+   IERC20(token).transfer(msg.sender, amount);
+ }
+}
---
+++ Get a Flashloan/contracts/IFlashloanUser.sol
@@ -0,0 +1,5 @@
+pragma solidity 0.7.3;
+
+interface IFlashloanUser {
+  function flashloanCallback(uint amount, address token, bytes memory data) external;
+}
