9397aac99d937cc1ee3887397d90b901db54f0ed
unknown <>
2021-07-26 14:10:08 +0530

    liquidity mining

---
+++ liquidity mining/contracts/GovernanceToken.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.7.3;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract GovernanceToken is ERC20, Ownable {
+  constructor() ERC20('Governance Token', 'GTK') Ownable() {}
+
+  function mint(address to, uint amount) external onlyOwner() {
+    _mint(to, amount);
+  }
+}
